e8522be6bca6707828e92ded4a14cc100d30261f
Loic Gardaire <loic.gardaire@etu.unice.fr>
2018-04-18 08:56:47 +0200

    change arraylist to vector to manage concurrency accesses

--- rmipubsubserver/src/main/java/fr/unice/polytech/garbou/rmipubsub/PubSubServerImpl.java
+++ rmipubsubserver/src/main/java/fr/unice/polytech/garbou/rmipubsub/PubSubServerImpl.java
@@ -1,40 +1,37 @@
 package fr.unice.polytech.garbou.rmipubsub;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * Created by Nassim B on 4/4/18.
  */
 public class PubSubServerImpl extends UnicastRemoteObject implements PubSubServer {
-	List<ClientHook> clients = new ArrayList<>();
+    List<ClientHook> clients = new Vector<>();
 
-	protected PubSubServerImpl() throws RemoteException {
-	}
+    protected PubSubServerImpl() throws RemoteException {
+    }
 
-	public void publish(String message) {
+    public void publish(String message) {
 
-		for (ClientHook client : this.clients) {
-			try {
-				client.receive(message);
-			} catch (RemoteException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        for (ClientHook client : this.clients) {
+            try {
+                client.receive(message);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public boolean subscribe(ClientHook client) throws RemoteException {
-			this.clients.add(client);
-			this.publish("A client joined : " + client);
-			return true;
-	}
+    public boolean subscribe(ClientHook client) throws RemoteException {
+        this.clients.add(client);
+        this.publish("A client joined : " + client);
+        return true;
+    }
 
     public boolean unsubscribe(ClientHook client) throws RemoteException {
-			return this.clients.remove(client);
+        return this.clients.remove(client);
     }
 }
